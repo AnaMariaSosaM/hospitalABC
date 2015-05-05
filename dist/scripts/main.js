2873d0dc6c73d58729bf2f335b4989d5bf25f1eb
Alekzander <alexlopezarroyave@gmail.com>
2015-05-05 11:57:49 -0500

    refactor search function and clean table, add schemas

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,145 +1,216 @@
 var patientsA, patientsB, isPatientA, isPatientB;
 //patients
-jQuery.ajax({
-	url: '/api/patients',
-	type: 'get',
-	dataType: 'json',
-	success: function (data) {
-		patientsA = data.patients.patient;
-	}
+$.ajax({
+  url: '/api/patients',
+  type: 'get',
+  dataType: 'json',
+
+  success: function (data) {
+    patientsA = data.patients.patient;
+  }
 });
 //patients1
-jQuery.ajax({
-	url: '/api/patients2',
-	type: 'get',
-	dataType: 'json',
-	success: function (data) {
-		patientsB = data.hospital.physician[0].patient;
-		patientsB = patientsB.concat(data.hospital.physician[1].patient)
+$.ajax({
+  url: '/api/patients2',
+  type: 'get',
+  dataType: 'json',
 
-	}
+  success: function (data) {
+    patientsB = data.hospital.physician[0].patient;
+    patientsB = patientsB.concat(data.hospital.physician[1].patient)
+  }
 });
 
-$('.buscarA').on('click', buscarA);
-$('.buscarB').on('click', buscarB);
-$('.comparar').on('click', comparar);
-$('.comun').on('click', comun);
-$('.soloUno').on('click', soloUno);
 
-function buscarA () {
-	$('.tablePatients').removeClass('hidden');
-	$.each(patientsA, function(index,val) {
-
-		if ($('.nombre').val() == val.name) {
-			isPatientA = true;
-			$('.name').text(val.name);
-			$('.gender').text(val.gender);
-			$('.ssn').text(val.ssn);
-			$('.date_of_birth').text(val.date_of_birth);
-			$('.address').text(val.address);
-			$('.zip_code').text(val.zip_code);
-			$('.city').text(val.city);
-			$('.state').text(val.state);
-		}
-
-	});
-	if (!isPatientA) {
-		alert('La persona que busca no se encuentra en la lista de contabilidad')
-	};
+function schemaClear (){
+  return{
+    name: "",
+    gender: "",
+    ssn: "",
+    dob: "",
+    address: "",
+    zip_code: "",
+    city: "",
+    state: "",
+  }
 }
-function buscarB () {
-	$.each(patientsB, function(index,val) {
-		if ($('.nombre').val() == val.first_name + " " + val.last_name ) {
-			isPatientB = true;
-			$('.name').text(val.first_name + " " + val.last_name );
-			$('.date_of_birth').text(val.dob)
-			$('.social_security').text(val.social_security)
-			$('.address').text(val.address)
-		}
-	});
-	if (!isPatientB) {
-		alert('La persona que busca no se encuentra en la lista de citas')
-	};
+
+function schemaA (val){
+  return{
+    name: val.name,
+    gender: val.gender,
+    ssn: val.ssn,
+    dob: val.date_of_birth,
+    address: val.address,
+    zip_code: val.zip_code,
+    city: val.city,
+    state: val.state,
+  }
 }
+
+
+function schemaB (val) {
+  return {
+    name: val.first_name + " " + val.last_name,
+    ssn: val.social_security,
+    dob: val.dob,
+    address: val.address
+  }
+}
+
+function foundUser (collection, input, schema){
+  // normalize input
+  var nameInput = input.toString().toLowerCase();
+  // obj to return
+  var foundedOBJ = false;
+
+  // loop looking the user
+  $.each(collection, function(index, val){
+    var nameFile = schema(val).name.toString().toLowerCase();
+
+    if( nameInput == nameFile ) {
+      foundedOBJ = schema(val);
+    }
+  });
+
+  // return schema or false
+  return foundedOBJ;
+}
+
+function showUserInTable (user) {
+  $('.name').text(user.name);
+  $('.gender').text(user.gender);
+  $('.ssn').text(user.ssn);
+  $('.date_of_birth').text(user.dob);
+  $('.address').text(user.address);
+  $('.zip_code').text(user.zip_code);
+  $('.city').text(user.city);
+  $('.state').text(user.state);
+  $('.tablePatients').removeClass('hidden');
+}
+
+function buscarAfn () {
+
+  var user = foundUser(patientsA, $('.nombre').val(), schemaA);
+
+  if (user) {
+    showUserInTable(user);
+  }else{
+    console.log('La persona que busca no se encuentra en la lista de contabilidad')
+  }
+}
+
+function buscarBfn () {
+  var user = foundUser(patientsB, $('.nombre').val(), schemaB);
+
+  if (user) {
+    showUserInTable(user);
+  }else{
+    console.log('La persona que busca no se encuentra en la lista de citas')
+  }
+}
+
 function comparar () {
 
-	var nombreA = '';
-	var direccionA = '';
-	var fechaA = '';
-	var socialA = '';
+  var nombreA = '';
+  var direccionA = '';
+  var fechaA = '';
+  var socialA = '';
 
-	var nombreB = '';
-	var direccionB = '';
-	var fechaB = '';
-	var socialB = '';
+  var nombreB = '';
+  var direccionB = '';
+  var fechaB = '';
+  var socialB = '';
 
-	$.each(patientsA, function(index,val) {
+  $.each(patientsA, function(index,val) {
 
-		if ($('.nombre').val() == val.name) {
-			nombreA = val.name;
-			direccionA = val.address;
-			fechaA = val.date_of_birth;
-			socialA = val.ssn;
-		}
+    if ($('.nombre').val() == val.name) {
+      nombreA = val.name;
+      direccionA = val.address;
+      fechaA = val.date_of_birth;
+      socialA = val.ssn;
+    }
 
-		console.log(nombreA + " "+ fechaA+ " "+ socialA , typeof fechaA);
-	});
+    console.log(nombreA + " "+ fechaA+ " "+ socialA , typeof fechaA);
+  });
 
-	$.each(patientsB, function(index,val) {
+  $.each(patientsB, function(index,val) {
 
-		if ($('.nombre').val() == val.first_name + " " + val.last_name ) {
-			nombreB = val.first_name + " " + val.last_name;
-			direccionB = val.address;
-			fechaB = val.dob;
-			socialB = val.social_security;
-		}
-		console.log(nombreB + " "+ fechaB+ " "+ socialB , typeof fechaB);
-	});
+    if ($('.nombre').val() == val.first_name + " " + val.last_name ) {
+      nombreB = val.first_name + " " + val.last_name;
+      direccionB = val.address;
+      fechaB = val.dob;
+      socialB = val.social_security;
+    }
+    console.log(nombreB + " "+ fechaB+ " "+ socialB , typeof fechaB);
+  });
 
-	if ($('.nombre').val().length === 0) {
-		alert('Write a patient name')
-	}else{
-		if (nombreA == nombreB && fechaA.toString() == fechaB.toString() && socialB.toString() == socialA.toString()) {
-			alert('equals')
-		}else{
-			alert('not equals')
-		}
-	}
+  if ($('.nombre').val().length === 0) {
+    alert('Write a patient name')
+  }else{
+    if (nombreA == nombreB && fechaA.toString() == fechaB.toString() && socialB.toString() == socialA.toString()) {
+      alert('equals')
+    }else{
+      alert('not equals')
+    }
+  }
 }
 function recorrer (a,b) {
-	$.each(patientsA, function(index,val) {
-		a.push(val.name.toString());
-	});
-	$.each(patientsB, function(indexB,valB) {
-		b.push(valB.first_name + " " + valB.last_name);
-	});
+  $.each(patientsA, function(index,val) {
+    a.push(val.name.toString());
+  });
+  $.each(patientsB, function(indexB,valB) {
+    b.push(valB.first_name + " " + valB.last_name);
+  });
 }
+
 function comun () {
-	$('.enComun').removeClass('hidden')
-	$('.listaEnComun').text('')
-	var patientsInA = [];
-	var patientsInB = [];
-	recorrer(patientsInA, patientsInB);
-	patientInBoth = _.intersection(patientsInA, patientsInB);
-	for (var i = 0; i < patientInBoth.length; i++) {
-		$('.listaEnComun').append(patientInBoth[i] + "<br>") ;
-	}	
+  $('.enComun').removeClass('hidden')
+  $('.listaEnComun').text('')
+  var patientsInA = [];
+  var patientsInB = [];
+  recorrer(patientsInA, patientsInB);
+  patientInBoth = _.intersection(patientsInA, patientsInB);
+  for (var i = 0; i < patientInBoth.length; i++) {
+    $('.listaEnComun').append(patientInBoth[i] + "<br>") ;
+  }
 }
+
 function soloUno () {
-	$('.listaDiferentes').text('');
-	$('.diferente').removeClass('hidden');
-	
-	var patientsInA = [];
-	var patientsInB = [];
-	recorrer(patientsInA, patientsInB);
-	
-	patientInOne = _.difference(_.union(patientsInA, patientsInB), _.intersection(patientsInA, patientsInB));
-	for (var i = 0; i < patientInOne.length; i++) {
-		$('.listaDiferentes').append(patientInOne[i] + "<br>") ;
-	}
+  var patientsInA = []
+    , patientsInB = [];
+
+  $('.listaDiferentes').text('');
+  $('.diferente').removeClass('hidden');
+
+  recorrer(patientsInA, patientsInB);
+
+  patientInOne = _.difference(_.union(patientsInA, patientsInB), _.intersection(patientsInA, patientsInB));
+
+  for (var i = 0; i < patientInOne.length; i++) {
+    $('.listaDiferentes').append(patientInOne[i] + "<br>") ;
+  }
 }
+
+function clearTable (argument) {
+  showUserInTable(schemaClear());
+}
+
 $(document).ready(function($) {
-	$('.tablePatients').addClass('hidden')
-	$('.enComun').addClass('hidden')
-	$('.diferente').addClass('hidden')
+  var $input = $(".nombre");
+  $('.tablePatients').addClass('hidden');
+  $('.enComun').addClass('hidden');
+  $('.diferente').addClass('hidden');
+
+  // Event listeners
+
+  // clean table every click
+  $('.buscarA, .buscarB').on('click', clearTable)
+
+  // actual listeners
+  $('.buscarA').on('click', buscarAfn);
+  $('.buscarB').on('click', buscarBfn);
+  $('.comparar').on('click', comparar);
+  $('.comun').on('click', comun);
+  $('.soloUno').on('click', soloUno);
 });
